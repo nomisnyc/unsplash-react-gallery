bbe98412490ee3598c723e0f8e9b9f38daf3e84e
Simon Wong <nomisnyc85@gmail.com>
2018-09-07 19:26:38 -0400

    change query param for unsplash to 16 per page

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -7,7 +7,7 @@ import ImageList from '../imagelist'
 import SearchForm from '../searchform'
 
 const AccessKey = API.API_ACCESS_KEY
-{/* stateful component. api call is done during the lifecycle of componentDidMount to avoid any bugs with api call. the doSearch function is part of the initial component rendering and search function from search bar. this was done so no code is repeated. there is a state for loading progress, in case the api call is lagging the user will see a loading response. 
+{/* stateful component. api call is done during the lifecycle of componentDidMount to avoid any bugs with api call. the doSearch function is part of the initial component rendering and search function from search bar. this was done so no code is repeated. there is a state for loading progress, in case the api call is lagging the user will see a loading response.
 */}
 class Gallery extends Component {
   constructor(props) {
@@ -24,7 +24,7 @@ class Gallery extends Component {
 
   doSearch = (query = 'New York') => {
     axios
-      .get('https://api.unsplash.com/search/photos/?page=1&per_page=10&query=' + query + '&client_id=' + AccessKey)
+      .get('https://api.unsplash.com/search/photos/?page=1&per_page=16&query=' + query + '&client_id=' + AccessKey)
       .then(data => {
         this.setState({ imgs: data.data.results, loading: false });
       })
